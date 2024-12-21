78f6b87a7184a6d75d817ffc3aa61b8ed9264b1b
bdvil <benjam.devillers@gmail.com>
2024-12-21 20:39:29 +0100

    Update monitor selection script to not fail when going from one to the other

--- hypr/select_monitor.sh
+++ hypr/select_monitor.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-eDP1Monitor="eDP-1"
-eDP1MonitorConfig="3840x2160@60, 0x0, 2"
-HDMIA2Monitor="HDMI-A-2"
-HDMIA2MonitorConfig="1920x1080@60, 0x1080, 1.2"
+mainMonitor="eDP-1"
+mainMonitorConfig="3840x2160@60, 0x0, 2"
+secondaryMonitor="HDMI-A-2"
+secondaryMonitorConfig="1920x1080@60, 0x1080, 1.2"
 
-echo "Only eDP-1|Only HDMI-A-2|Both" | wofi --show dmenu -Ddmenu-separator='|' -Ddmenu-print_line_num=true | {
+echo "Only Main Monitor|Only Second Monitor|Both Monitors" | wofi --show dmenu -Ddmenu-separator='|' -Ddmenu-print_line_num=true | {
     out=(
-        "hyprctl keyword monitor $HDMIA2Monitor,disable && hyprctl keyword monitor $eDP1Monitor,$eDP1MonitorConfig"
-        "hyprctl keyword monitor $eDP1Monitor,disable && hyprctl keyword monitor $HDMIA2Monitor,$HDMIA2MonitorConfig" 
-        "hyprctl keyword monitor $eDP1Monitor,$eDP1MonitorConfig && hyprctl keyword monitor $HDMIA2Monitor,$HDMIA2MonitorConfig"
+        "hyprctl keyword monitor $mainMonitor,$mainMonitorConfig && hyprctl keyword monitor $secondaryMonitor,disable"
+        "hyprctl keyword monitor $secondaryMonitor,$secondaryMonitorConfig && hyprctl keyword monitor $mainMonitor,disable" 
+        "hyprctl keyword monitor $mainMonitor,$mainMonitorConfig && hyprctl keyword monitor $secondaryMonitor,$secondaryMonitorConfig"
     )
     read -r item
     if ! [[ $item == "" ]]; then
